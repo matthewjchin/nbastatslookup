425fa02b595fc52b30f1de89c7e8c695160dd8f5
Matthew Chin <mattchin813@gmail.com>
2024-02-23 10:23:29 -0800

    basic frontend interface

--- src/app.py
+++ src/app.py
@@ -5,45 +5,45 @@ from nba_api.stats.static import players
 
 app = Flask(__name__)
 
-#
-# @app.route("/echo_input", methods=["POST"])
-# def echo_input():
-#     input_text = request.form.get("user_input", "")
-#     return "Greeting: " + input_text
-
-
 
 @app.route("/echo_user_input", methods=["POST"])
-def get_player_name_active():
+def echo_input():
+    input_text = request.form.get("user_input", "")
+    return "Greeting: " + input_text
 
-    user_input = request.form["user_input"]
 
-    nba_player = players.find_players_by_full_name(user_input)
-    if user_input != nba_player[0]['full_name']:
-        return "The player cannot be found. Please go back and try again."
-    else:
-        nba_player_career = playercareerstats.PlayerCareerStats(player_id=nba_player[0]['id'])
-        return nba_player_career.get_normalized_json()
+# @app.route("/echo_user_input", methods=["POST"])
+# def get_player_name_active():
+#
+#     user_input = request.form["user_input"]
+#
+#     nba_player = players.find_players_by_full_name(user_input)
+#     if user_input != nba_player[0]['full_name']:
+#         return "The player cannot be found. Please go back and try again."
+#     else:
+#         nba_player_career = playercareerstats.PlayerCareerStats(player_id=nba_player[0]['id'])
+#         return nba_player_career.get_normalized_json()
 
 
 @app.route("/")
 def main():
-
     return '''Soon this will be a website for NBA basketball players' metrics for stats gurus,
         fantasy players, or curiosity.
         <br><br>
 
-    You can check if the player you entered is active or not.
-
-    Enter the first AND last name and spell correctly. <br>
-
-    NOTE: We are working to get a drop down menu and/or features to search by last name.
-
-    </p>
-
+    In the meantime, enter a greeting. 
+    
      <form action="/echo_user_input" method="POST">
          <input name="user_input">
          <input type="submit" value="Submit!">
      </form>
     '''
 
+# '''You can check if the player you entered is active or not.
+#
+#     Enter the first AND last name and spell correctly. <br>
+#
+#     NOTE: We are working to get a drop down menu and/or features to search by last name.
+#
+#     </p>
+# '''
