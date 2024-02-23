a7139983103207df3d6aadcd3611498a0db6828c
Matthew Chin <mattchin813@gmail.com>
2024-02-23 15:33:36 -0800

    add more test files

---
+++ test_app.py
@@ -0,0 +1,13 @@
+import unittest
+from app import get_player_name_active
+
+
+class MyTestCase(unittest.TestCase):
+    def test_get_player_name_active(self):
+        self.assertFalse(get_player_name_active())
+        self.assertFalse(get_player_name_active(""))
+        self.assertTrue(get_player_name_active())
+
+
+if __name__ == '__main__':
+    unittest.main()
