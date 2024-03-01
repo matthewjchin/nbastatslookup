de8c9afb4e92115d5a625efd29a26df478b5fe68
Matthew Chin <mattchin813@gmail.com>
2024-02-29 21:45:04 -0800

    ppg only

--- src/app.py
+++ src/app.py
@@ -65,22 +65,22 @@ def get_active_player_avgs():
 
     # Get player points, rebounds, assists, steals, blocks, percentages per game
     player_ppg = get_points_per_game(player[0]['id'])
-    player_rpg = get_rebounds_per_game(player[0]['id'])
-    player_apg = get_assists_per_game(player[0]['id'])
-    player_spg = get_steals_per_game(player[0]['id'])
-    player_bpg = get_blocks_per_game(player[0]['id'])
-    player_fg = get_fg_pct_per_game_career(player[0]['id'])
-    player_3pg = get_3pfg_pct_per_game_career(player[0]['id'])
-    player_ft = get_ft_pct_per_game_career(player[0]['id'])
+    # player_rpg = get_rebounds_per_game(player[0]['id'])
+    # player_apg = get_assists_per_game(player[0]['id'])
+    # player_spg = get_steals_per_game(player[0]['id'])
+    # player_bpg = get_blocks_per_game(player[0]['id'])
+    # player_fg = get_fg_pct_per_game_career(player[0]['id'])
+    # player_3pg = get_3pfg_pct_per_game_career(player[0]['id'])
+    # player_ft = get_ft_pct_per_game_career(player[0]['id'])
 
     career_avgs += "\nPoints per game:\t" + str(player_ppg)
-    career_avgs += "\nRebounds per game: \t" + str(player_rpg)
-    career_avgs += "\nAssists per game: \t" + str(player_apg)
-    career_avgs += "\nSteals per game: \t" + str(player_spg)
-    career_avgs += "\nBlocks per game: \t" + str(player_bpg)
-    career_avgs += "\nCareer FG Percentage: \t" + str(player_fg)
-    career_avgs += "\nCareer 3PFG Percentage: \t" + str(player_3pg)
-    career_avgs += "\nCareer FT Percentage: \t" + str(player_ft)
+    # career_avgs += "\nRebounds per game: \t" + str(player_rpg)
+    # career_avgs += "\nAssists per game: \t" + str(player_apg)
+    # career_avgs += "\nSteals per game: \t" + str(player_spg)
+    # career_avgs += "\nBlocks per game: \t" + str(player_bpg)
+    # career_avgs += "\nCareer FG Percentage: \t" + str(player_fg)
+    # career_avgs += "\nCareer 3PFG Percentage: \t" + str(player_3pg)
+    # career_avgs += "\nCareer FT Percentage: \t" + str(player_ft)
 
     return career_avgs
 
